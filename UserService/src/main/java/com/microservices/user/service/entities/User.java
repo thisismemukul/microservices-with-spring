3d91d26840fcc02fd4fd13d8fafb544aec0e5278
thisismemukul <w41k3r.12@gmail.com>
2023-03-28 12:40:05 +0530

    Step_01 Created User Entity

---
+++ UserService/src/main/java/com/microservices/user/service/entities/User.java
@@ -0,0 +1,21 @@
+package com.microservices.user.service.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="micro_user")
+public class User {
+
+    @Id
+    @Column(name="ID")
+    private String userId;
+    @Column(name="NAME",nullable = false, length = 20)
+    private String name;
+    @Column(name = "EMAIL",nullable = false,length =20)
+    private String email;
+    @Column(name = "ABOUT",length = 70)
+    private String about;
+}
