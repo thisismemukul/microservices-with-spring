faab200f01f64d3a3bfdd8dfb7513bd5fdb3d22c
thisismemukul <w41k3r.12@gmail.com>
2023-03-28 20:49:31 +0530

    Step_05 Created Controllers, createUser methodd

---
+++ UserService/src/main/java/com/microservices/user/service/controllers/UserController.java
@@ -0,0 +1,27 @@
+package com.microservices.user.service.controllers;
+
+import com.microservices.user.service.entities.User;
+import com.microservices.user.service.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+    //create
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User createdUser = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+    //single user get
+
+    //all user get
+}
