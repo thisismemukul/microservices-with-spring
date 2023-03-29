9dbcdd8997abbe17a214cc8aca4edb38a8a5914e
thisismemukul <w41k3r.12@gmail.com>
2023-03-29 16:58:48 +0530

    Step_07 Created getAllHotels method Controller in userController

--- HotelService/src/main/java/com/microservices/hotel/controllers/HotelController.java
+++ HotelService/src/main/java/com/microservices/hotel/controllers/HotelController.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/hotels")
 public class HotelController {
@@ -23,5 +25,10 @@ public class HotelController {
         return ResponseEntity.ok(hotel);
     }
     //get all
+    @GetMapping
+    public ResponseEntity<List<Hotel>> getAllHotels(){
+        List<Hotel> allUsers = hotelService.getAll();
+        return ResponseEntity.ok(allUsers);
+    }
 
 }
