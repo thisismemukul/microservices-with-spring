7dbbced8b1301361c01ab6b689d75c01400b7805
thisismemukul <w41k3r.12@gmail.com>
2023-03-30 00:22:07 +0530

    initial commit

--- UserService/src/main/java/com/microservices/user/service/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.microservices.user.service.repositories;
-
-import com.microservices.user.service.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,String> {
-}
