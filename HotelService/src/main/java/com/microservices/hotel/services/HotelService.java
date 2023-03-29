076060430ff455f2435621cde24a4c27f699213d
thisismemukul <w41k3r.12@gmail.com>
2023-03-29 16:20:02 +0530

    Step_03 Created Services package HotelService Interface

---
+++ HotelService/src/main/java/com/microservices/hotel/services/HotelService.java
@@ -0,0 +1,16 @@
+package com.microservices.hotel.services;
+
+import com.microservices.hotel.entities.Hotel;
+
+import java.util.List;
+
+public interface HotelService {
+    //create
+    Hotel create(Hotel hotel);
+    //getAll
+    List<Hotel>getAll();
+    //get single
+    Hotel get(String id);   
+
+    //TODO: delete
+}
