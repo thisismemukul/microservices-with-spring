d088f7f03247afe18da6f4fcee28553d2239b116
thisismemukul <w41k3r.12@gmail.com>
2023-03-29 17:45:15 +0530

    final commit

--- HotelService/src/main/java/com/microservices/hotel/controllers/HotelController.java
+++ HotelService/src/main/java/com/microservices/hotel/controllers/HotelController.java
@@ -2,6 +2,7 @@ package com.microservices.hotel.controllers;
 
 import com.microservices.hotel.entities.Hotel;
 import com.microservices.hotel.services.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/hotels")
 public class HotelController {
+    @Autowired
     private HotelService hotelService;
     //create
     @PostMapping
